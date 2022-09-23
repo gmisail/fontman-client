755f532f7315821bfe489a5fe172a836fd14c040
Graham Misail <gmisail64@gmail.com>
2022-09-23 14:27:53 -0400

    Add 'force' flag to cache command

--- cmd/fontman/commands/cache.go
+++ cmd/fontman/commands/cache.go
@@ -7,17 +7,30 @@ import (
 )
 
 // Called if 'cache' subcommand is invoked.
-func onCache(c *cli.Context) error {
+func onCache(c *cli.Context, force bool) error {
 	fmt.Println("list some fonts...")
+	fmt.Println(force)
 
 	return nil
 }
 
 // Constructs the 'cache' subcommand.
 func RegisterCache() *cli.Command {
+	var force bool
+
 	return &cli.Command{
-		Name:   "cache",
-		Usage:  "Regenerate the cache for currently installed fonts.",
-		Action: onCache,
+		Name:  "cache",
+		Usage: "Regenerate the cache for currently installed fonts.",
+		Action: func(c *cli.Context) error {
+			return onCache(c, force)
+		},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "force",
+				Aliases:     []string{"f"},
+				Usage:       "force",
+				Destination: &force,
+			},
+		},
 	}
 }
