a568d7b84c897cb805810df7ea544162b0a563c8
Graham Misail <gmisail64@gmail.com>
2022-10-13 15:46:50 -0400

    Add basic model for FontFamily, FontStyle

--- pkg/model/project.go
+++ pkg/model/project.go
@@ -1,11 +1,11 @@
 package model
 
 type ProjectFont struct {
-	Name string `yaml:"name"`
+	Name   string   `yaml:"name"`
 	Styles []string `yaml:"styles"`
 }
 
 type ProjectFile struct {
-	Name string `yaml:"name"` 
+	Name  string        `yaml:"name"`
 	Fonts []ProjectFont `yaml:"fonts"`
 }
