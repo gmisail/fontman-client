6d21f70c5be24fd30f64dcbe5ef010cc31863060
Graham Misail <gmisail64@gmail.com>
2022-10-13 20:58:48 -0400

    Add tests for line parser

--- go.mod
+++ go.mod
@@ -4,13 +4,18 @@ go 1.19
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/goccy/go-yaml v1.9.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/urfave/cli/v2 v2.16.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/parser/list.go
+++ pkg/parser/list.go
@@ -2,11 +2,15 @@ package parser
 
 import (
 	"fontman/client/pkg/model"
+	"path/filepath"
 	"strings"
 )
 
+/*
+	Split the line into three sections: path, family name, and styles
+*/
 func SplitSections(line string) []string {
-	sections := strings.Split(line, ":")
+	sections := strings.Split(strings.TrimSpace(line), ":")
 
 	for i, section := range sections {
 		sections[i] = strings.TrimSpace(section)
@@ -15,25 +19,51 @@ func SplitSections(line string) []string {
 	return sections
 }
 
+func ParseListLines(lines string) []model.FontFamily {
+	families := []model.FontFamily{}
+
+	for _, line := range strings.Split(lines, "\n") {
+		families = append(families, ParseListLine(line))
+	}
+
+	return families
+}
+
+/*
+	Split all families into a list of names
+*/
+func ParseFamilyNames(fullName string) []string {
+	return strings.Split(fullName, ",")
+}
+
+/*
+	Parse a line into a FontFamily model
+*/
 func ParseListLine(line string) model.FontFamily {
 	sections := SplitSections(line)
 
-	// path := sections[0]
-	styles := ParseStyles(sections[2])
+	path := sections[0]
+	fontFamilyNames := ParseFamilyNames(sections[1])
+	fontFormat := filepath.Ext(path)
+	styles := ParseStyles(path, fontFormat, sections[2])
 
 	return model.FontFamily{
-		Name: "family",
-		Styles: styles,
+		Name:     fontFamilyNames[0],
+		Styles:   styles,
 		Language: "en",
 	}
 }
 
-func ParseFontPath(path string) {
+/*
+	Parse a style section into FontStyle models
+*/
+func ParseStyles(filePath string, fileFormat string, styles string) []model.FontStyle {
+	styleNames := strings.Split(strings.TrimLeft(styles, "style="), ",")
+	var fontStyles []model.FontStyle
 
-}
+	for _, name := range styleNames {
+		fontStyles = append(fontStyles, model.FontStyle{Name: name, Path: filePath, Format: fileFormat})
+	}
 
-func ParseStyles(path string, styles string) []model.FontStyle {
-	styleNames := strings.Split(strings.Trim(styles, "style="), ",")
-
-	
+	return fontStyles
 }
