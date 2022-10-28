44c678b8d5329d487fd6d9f7a9cdc3ce0ac68f5b
meow <fatmeowxd@pm.me>
2022-10-28 15:32:29 -0400

    Function to detect if the program is being run with root.

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -81,3 +81,10 @@ func GetInstallationPath() string {
 
 	return filepath.Join(dir, "/.fontman")
 }
+
+func GetUser() bool {
+	// os.Geteuid() should be sufficient in detecting if the user running it has root permission
+	// SUDO_USER is not reliable as attacker could potentially set the environmental variable
+	isRoot := os.Geteuid()
+	return isRoot == 0
+}
