3c7c70ab0b5e7c7ee981b8a14c0343a8a3bc9b58
Graham Misail <gmisail64@gmail.com>
2022-12-09 00:36:10 -0500

    Move reading logic to model

--- pkg/model/registry.go
+++ pkg/model/registry.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
 type RegistryFile struct {
 	Name    string          `yaml:"name"`
 	License string          `yaml:"license"`
@@ -10,4 +16,20 @@ type RegistryFile struct {
 type RegistryStyle struct {
 	Name string `yaml:"name"`
 	Url  string `yaml:"url"`
+}
+
+func ReadRegistryFile(path string) (*RegistryFile, error) {
+	contents, fileErr := os.ReadFile(path)
+
+	if fileErr != nil {
+		return nil, fileErr
+	}
+
+	var registryFile RegistryFile
+
+	if parseErr := yaml.Unmarshal(contents, &registryFile); parseErr != nil {
+		return nil, parseErr
+	}
+
+	return &registryFile, nil
 }
--- pkg/service/registry.go
+++ pkg/service/registry.go
@@ -2,23 +2,8 @@ package service
 
 import (
 	"fontman/client/pkg/model"
-	"os"
-
-	"github.com/goccy/go-yaml"
 )
 
-func ReadRegistryFile(path string) (*model.RegistryFile, error) {
-	contents, fileErr := os.ReadFile(path)
-
-	if fileErr != nil {
-		return nil, fileErr
-	}
-
-	var registryFile model.RegistryFile
-
-	if parseErr := yaml.Unmarshal(contents, &registryFile); parseErr != nil {
-		return nil, parseErr
-	}
-
-	return &registryFile, nil
+func UploadRegistryFile(file model.RegistryFile) error {
+	
 }
